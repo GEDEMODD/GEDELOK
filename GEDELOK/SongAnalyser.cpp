5ac494c1eb58a6d4c71e0ac9cef634af607cad90
arnarkari93 <arnarkari93@gmail.com>
2016-03-29 21:43:44 +0000

    Added particlebeat class

--- GEDELOK/SongAnalyser.cpp
+++ GEDELOK/SongAnalyser.cpp
@@ -26,7 +26,7 @@ SongAnalyser::SongAnalyser(SceneManager* sceneManager)
 	if (!BASS_Init(-1,44100,0,NULL,NULL)) Ogre::LogManager::getSingleton().logMessage("can't initialize BASS");
 	else
 	{
-		char file[MAX_PATH]="../dab.mp3";
+		char file[MAX_PATH]="../wave.mp3";
 		if (!(chan=BASS_StreamCreateFile(FALSE,file,0,0,BASS_SAMPLE_LOOP))
 			&& !(chan=BASS_MusicLoad(FALSE,file,0,0,BASS_MUSIC_RAMP|BASS_SAMPLE_LOOP,0))) 
 			Ogre::LogManager::getSingleton().logMessage("can't play music file");
@@ -45,16 +45,22 @@ SongAnalyser::~SongAnalyser()
 void SongAnalyser::update()
 {
 	// analyse channel data
-	float fft[1024];
-	BASS_ChannelGetData(chan,fft,BASS_DATA_FFT2048); // get the FFT data
+	float fft[128];
+	BASS_ChannelGetData(chan,fft,BASS_DATA_FFT256); // get the FFT data
 	int y=0, b0=0;
 
-	for (int x = 0; x<BANDS; x++) {
+	for (int i = 0; i < 128; i++) {
+		logFile << fft[i];
+		logFile << "; ";
+	}
+	logFile << "\n";
+
+	for (int x = 0; x < BANDS; x++) {
 		float sum = 0;
 		int b1 = pow(2, x * 10.0 / (BANDS-1));
 		
-		if (b1 > 1023) {
-			b1 = 1023;
+		if (b1 > 127) {
+			b1 = 127;
 		}
 		
 		if (b1 <= b0) {
@@ -66,7 +72,7 @@ void SongAnalyser::update()
 			sum += fft[1 + b0];
 		}
 		y = (sqrt(sum / log10((float)sc)) * 1.7 * SPECHEIGHT) - 4; // scale it
-		if (y>SPECHEIGHT) { 
+		if (y > SPECHEIGHT) { 
 			y = SPECHEIGHT; // cap it
 		}
 
@@ -74,8 +80,8 @@ void SongAnalyser::update()
 		// y will range from 0 (sometimes negative) to SPECHEIGHT
 		// now let's visualize it with cubes
 		Vector3 currentpos = cubes[x]->getPosition();
-		cubes[x]->setScale(0.3, (float)y / 50.0, 0.3);
-		cubes[x]->setPosition(currentpos.x, 0.5 * 106.08 * (float)y / 50.0 , currentpos.z);
+		cubes[x]->setScale(0.1, (float)y / 50.0, 0.1);
+		cubes[x]->setPosition(currentpos.x, 53.04 * (float)y / 50.0 , currentpos.z);
 	}
 }
 
@@ -91,15 +97,24 @@ void SongAnalyser::notify()
 	BASS_ChannelGetData(chan, fft, BASS_DATA_FFT2048); // get the FFT data
 	int y = 0, b0 = 0;
 	DWORD val = BASS_ChannelGetLevel(chan);
+
 	for(unsigned int x = 0; x < observers.size(); x++) {
 		SceneNode *curr = observers[x];
 
-		logFile << val;
-		logFile << "\n";
-		long value = val * 0.00000001; 
-		if ( value >= 0.8) {
+		//logFile << val;
+		//logFile << "\n";
+		long value = fft[90];
+		if ( curr->getScale().x > value) {
 			curr->setPosition(curr->getPosition().x, value,  curr->getPosition().z);
-			curr->setScale(value, value, value);
+			curr->setScale(curr->getScale().x - 0.1, curr->getScale().y - 0.1,  curr->getScale().z - 0.1);
+		} else {
+			curr->setPosition(curr->getPosition().x, value,  curr->getPosition().z);
+			curr->setScale(curr->getScale().x + 0.1, curr->getScale().y + 0.1,  curr->getScale().z + 0.1);
+		}
+
+		if ( value > 2 ) 
+		{
+
 		}
 	}
 	/*for (int i = 0; fft[i] != '\0'; i++) {
